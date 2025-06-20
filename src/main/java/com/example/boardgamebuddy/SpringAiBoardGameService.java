4e6eb78af814cf0770c20e1cb3f5f612bbf22f26
najariannareg <43207695+najariannareg@users.noreply.github.com>
2025-06-20 14:36:05 +0400

    Chapter 3.1. Working with prompt templates.

--- src/main/java/com/example/boardgamebuddy/SpringAiBoardGameService.java
+++ src/main/java/com/example/boardgamebuddy/SpringAiBoardGameService.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.evaluation.RelevancyEvaluator;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.evaluation.EvaluationRequest;
 import org.springframework.ai.evaluation.EvaluationResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,9 @@ public class SpringAiBoardGameService implements BoardGameService {
 
     private final ChatClient chatClient;
     private final RelevancyEvaluator evaluator;
+
+    @Value("classpath:/prompttemplates/questionPromptTemplate.st")
+    Resource questionPromptTemplate;
 
     public SpringAiBoardGameService(ChatClient.Builder chatClientBuilder) {
         ChatOptions chatOptions = ChatOptions.builder()
@@ -32,16 +37,15 @@ public class SpringAiBoardGameService implements BoardGameService {
     @Override
     @Retryable(retryFor = AnswerNotRelevantException.class)
     public Answer askQuestion(Question question) {
-        String prompt = "Answer this question about " + question.gameTitle() +
-                ": " + question.question();
-
         String answerText = chatClient.prompt()
-                .user(prompt)
+                .user(spec -> spec
+                        .text(questionPromptTemplate)
+                        .param("gameTitle", question.gameTitle())
+                        .param("question", question.question()))
                 .call()
                 .content();
 
         Answer answer = new Answer(question.gameTitle(), answerText);
-
         evaluateRelevancy(question, answer);
 
         return answer;
