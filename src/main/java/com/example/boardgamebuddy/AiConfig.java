e49e023b278c3f5954071a0c42a5d4124aa4d413
najariannareg <43207695+najariannareg@users.noreply.github.com>
2025-06-23 19:02:47 +0400

    Chapter 5 Chat Memory.

--- src/main/java/com/example/boardgamebuddy/AiConfig.java
+++ src/main/java/com/example/boardgamebuddy/AiConfig.java
@@ -1,7 +1,9 @@
 package com.example.boardgamebuddy;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
@@ -10,13 +12,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AiConfig {
 
+//    @Bean
+//    ChatClient chatClient(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
+//        return chatClientBuilder
+//                .defaultOptions(ChatOptions.builder()
+//                        .model("gpt-4o-mini")
+//                        .build())
+//                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
+//                .build();
+//    }
+
     @Bean
-    ChatClient chatClient(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
+    ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
         return chatClientBuilder
                 .defaultOptions(ChatOptions.builder()
                         .model("gpt-4o-mini")
                         .build())
-                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
     }
 
