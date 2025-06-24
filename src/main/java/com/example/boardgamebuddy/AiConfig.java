09930df327a5ec3801166c29ad7275c8338c5666
najariannareg <43207695+najariannareg@users.noreply.github.com>
2025-06-24 10:56:06 +0400

    Chapter 5.5.2 Storing chat memory in a vector store.

--- src/main/java/com/example/boardgamebuddy/AiConfig.java
+++ src/main/java/com/example/boardgamebuddy/AiConfig.java
@@ -3,6 +3,7 @@ package com.example.boardgamebuddy;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.VectorStoreChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -22,19 +23,29 @@ public class AiConfig {
 //                .build();
 //    }
 
+//    @Bean
+//    ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+//        return chatClientBuilder
+//                .defaultOptions(ChatOptions.builder()
+//                        .model("gpt-4o-mini")
+//                        .build())
+//                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+//                .build();
+//    }
+//
+//    @Bean
+//    ChatMemory chatMemory(ConversationRepository repository) {
+//        return new MongoChatMemory(repository);
+//    }
+
     @Bean
-    ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    ChatClient chatClient(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
         return chatClientBuilder
                 .defaultOptions(ChatOptions.builder()
                         .model("gpt-4o-mini")
                         .build())
-                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .defaultAdvisors(VectorStoreChatMemoryAdvisor.builder(vectorStore).build())
                 .build();
-    }
-
-    @Bean
-    ChatMemory chatMemory(ConversationRepository repository) {
-        return new MongoChatMemory(repository);
     }
 
 }
